654451f9cf556846ae3ac9f8303d653ccecf2be3
Joshua Evan Arijanto <joshua.arijanto@gmail.com>
2020-07-15 23:15:39 +0700

    add seeder for default data of categories

---
+++ findcomputer-backend/findcomputer/src/main/resources/db/migration/V5__SeedCategories.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (name) VALUES('RAM');
+INSERT INTO categories (name) VALUES('Processor');
+INSERT INTO categories (name) VALUES('VGA');
+INSERT INTO categories (name) VALUES('Motherboard');
+INSERT INTO categories (name) VALUES('Storage');
+
